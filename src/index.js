bab8707072f07b24f942cabef0a0722579d79815
Shuo Yang <shuo.yang.tek@gmail.com>
2017-10-08 06:10:04 +0800

    fix

--- src/index.js
+++ src/index.js
@@ -2,4 +2,6 @@ module.exports = {
 	delay: require('./util/delay'),
 	request: require('./util/request'),
 	followLocation: require('./util/follow-location'),
+	makeError: require('./util/make-error'),
+	makeResult: require('./util/make-result'),
 }
